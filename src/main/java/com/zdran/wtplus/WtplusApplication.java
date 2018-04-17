61a2ea8cc0e6e79478c1b34511e8b790dae67f69
ranzhendong <ranzd@chianunicom.cn>
2018-04-17 12:42:01 +0800

    创建项目

---
+++ src/main/java/com/zdran/wtplus/WtplusApplication.java
@@ -0,0 +1,12 @@
+package com.zdran.wtplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtplusApplication.class, args);
+	}
+}
